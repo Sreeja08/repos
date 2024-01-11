dc92b189c6519aa25c2e2ee58a4cbf0926460c4e
Sreeja Thallada <103803255+Sreeja08@users.noreply.github.com>
2024-01-11 20:57:28 +0530

    Update Todo.java

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -7,11 +7,11 @@ public class Todo {
     private String status;
     private String priority;
 
-    public Todo(int id, String todo, String status, String priority) {
+    public Todo(int id, String todo, String priority, String status) {
         this.id = id;
         this.todo = todo;
-        this.status = status;
         this.priority = priority;
+        this.status = status;
     }
 
     public int getId() {
